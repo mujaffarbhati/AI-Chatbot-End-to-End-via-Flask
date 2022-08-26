5863fa04f3d376e95cfd88c1f388c05c1210f7af
Bhati Mujaffar <mujaffarbhati444@gmail.com>
2022-08-26 18:39:41 +0530

    Loading the pickle

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import torch
-from transformers import BertForQuestionAnswering 
-from transformers import BertTokenizer
+# from transformers import BertForQuestionAnswering 
+# from transformers import BertTokenizer
 import pickle
 
 
-model= BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-save_model = open(r"model.pickle","wb")
-pickle.dump(model, save_model)
-save_model.close()
+# model= BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+# save_model = open(r"model.pickle","wb")
+# pickle.dump(model, save_model)
+# save_model.close()
 
-tokenizer= BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-save_tokenizer = open(r"tokenizer.pickle","wb")
-pickle.dump(tokenizer,save_tokenizer)
-save_tokenizer.close()
+# tokenizer= BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+# save_tokenizer = open(r"tokenizer.pickle","wb")
+# pickle.dump(tokenizer,save_tokenizer)
+# save_tokenizer.close()
+
+open_file = open("model.pickle", "rb")
+model= pickle.load(open_file)
+open_file.close()
+
+open_file = open("tokenizer.pickle", "rb")
+tokenizer= pickle.load(open_file)
+open_file.close()
+
+
 
 
